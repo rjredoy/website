02af8e58d06890da1b1d925d6c596588eb729306
rjredoy <66143428+rjredoy@users.noreply.github.com>
2020-06-06 00:47:22 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*****aos animation text*****/
+AOS.init();
+/*****aos animation text*****/
+
+
+
+/*****slick slider text*****/
+$('.slide').slick({
+	dots:true,
+});
+/*****slick slider text*****/
